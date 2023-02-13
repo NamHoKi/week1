4502f4863daefd73c57cf08ab5e94d9b73c31506
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2023-02-14 01:55:32 +0900

    Update gui_v2.py

--- gui_v2.py
+++ gui_v2.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon, QPixmap, QImage
 
@@ -13,7 +14,7 @@ import time
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.start_time = time.time()
         # device setting
         self.DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -48,32 +49,22 @@ class MyWindow(QMainWindow):
         self.setStyleSheet("color: white;"
                         "background-color: #333333")
 
-        ## Text : "Upload"
-        # label1 = QLabel(self)
-        # label1.setText('Upload')
-        # label1.move(10, 0)
-        # label1.setStyleSheet("color: red;"
-        #                      "border-style: solid;"
-        #                      "border-width: 2px;"
-        #                      "border-color: #FA8072;"
-        #                      "border-radius: 3px")
-
         # Button 1
-        btn1 = QPushButton("MODEL", self)
+        btn1 = QPushButton("MODEL\nload", self)
         btn1.setStyleSheet("color: white;"
                         "background-color: #444444")
         btn1.move(10, 10)
         btn1.resize(130, 50)
         btn1.clicked.connect(self.set_model)
         # Button 2
-        btn2 = QPushButton("IMAGE", self)
+        btn2 = QPushButton("IMAGE\nload", self)
         btn2.setStyleSheet("color: white;"
                            "background-color: #444444")
         btn2.move(10, 80)
         btn2.resize(130, 50)
         btn2.clicked.connect(self.show_image)
         # Button 3
-        btn3 = QPushButton("MP4", self)
+        btn3 = QPushButton("MP4\nload", self)
         btn3.setStyleSheet("color: white;"
                            "background-color: #444444")
         btn3.move(10, 150)
@@ -81,14 +72,32 @@ class MyWindow(QMainWindow):
         btn3.clicked.connect(self.play_mp4)
         self.mp4_stop = False
         # Button 4
-        btn4 = QPushButton("STOP", self)
+        btn4 = QPushButton("MP4\nstop", self)
         btn4.setStyleSheet("color: white;"
                            "background-color: #444444")
         btn4.move(10, 220)
         btn4.resize(130, 50)
         btn4.clicked.connect(self.MP4_Stop)
 
-        # image window
+        # Line edit
+        self.line_edit1 = QLineEdit(self)
+        self.line_edit1.move(10, 370)
+        self.line_edit1.resize(130, 50)
+        # Text label
+        self.text_label1 = QLabel(self)
+        self.text_label1.move(10, 440)
+        self.text_label1.setText('Your E-mail')
+        self.text_label1.resize(130, 50)
+        # Button 5
+        btn5 = QPushButton(self)
+        btn5.resize(130, 50)
+        btn5.setStyleSheet("color: white;"
+                           "background-color: #444444")
+        btn5.move(10, 510)
+        btn5.setText('OK')
+        btn5.clicked.connect(self.button_event)
+
+        # Image window
         self.pixmap = QPixmap('image/black.png')
         self.image_label = QLabel(self)
         self.image_label.setPixmap(self.pixmap)  # 이미지 세팅
@@ -100,6 +109,20 @@ class MyWindow(QMainWindow):
         self.sb = self.statusBar()
         self.setStatusBar(self.sb)
         self.sb.showMessage('None')
+
+        # Scroll Bar
+        self.scrollArea1 = QScrollArea(self)
+        self.scroll_label = QLabel('time - ' + str(time.time() - self.start_time) + '; GUI console : ' + 'Start MS AI School Team 1 Project')
+        self.scroll_label.setStyleSheet("color: black;"
+                           "background-color: #ffffff")
+        self.scroll_label.resize(1200, 20000)
+        self.scroll_label.setAlignment(Qt.AlignTop)
+        self.scrollArea1.setWidget(self.scroll_label)
+        self.scrollArea1.move(10, 650)
+        self.scrollArea1.resize(1260, 130)
+        self.scrollArea1.setStyleSheet("color: white;"
+                           "background-color: #ffffff")
+
         self.show()
 
 
@@ -145,20 +168,22 @@ class MyWindow(QMainWindow):
     #     return tab
 
 
-
     def set_model(self):
         fname = QFileDialog.getOpenFileName(self)
         if fname[0] != '' and fname[0][-3:] == '.pt' :
             QMessageBox.information(self, 'Info', 'Wait for Model setting')
+            self.add_gui_console('UPLOADING : Wait for uploading model ...')
             self.sb.showMessage('UPLOADING : Wait for uploading model ...')
             self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=fname[0])
             self.model.conf = 0.5  # NMS confidence threshold
             self.model.iou = 0.45  # NMS IoU threshold
             self.model.to(self.DEVICE)
             self.sb.showMessage('COMPLETE  --- Model setting')
+            self.add_gui_console('COMPLETE  : Model setting')
             QMessageBox.information(self, 'Info', 'COMPLETE  : Model setting')
         else :
             self.sb.showMessage('WARNING   --- Not Found (*.pt) File')
+            self.add_gui_console('WARNING   --- Not Found (*.pt) File')
             QMessageBox.information(self, 'Warning', 'WARNING   : Not Found (*.pt) File')
 
 
@@ -186,7 +211,7 @@ class MyWindow(QMainWindow):
                 cv2.putText(image, str(round(score, 4)), (int(x1), int(y1 - 25)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (0, 0, 255), 2)
             except Exception as e:
-                print(e)
+                self.add_gui_console(str(e))
 
         return image
 
@@ -196,6 +221,7 @@ class MyWindow(QMainWindow):
             fname = QFileDialog.getOpenFileName(self)
             if fname[0] != '' and (fname[0][-4:] == '.jpg' or fname[0][-4:] == '.png') :
                 image = cv2.imread(fname[0])
+                self.add_gui_console('IMAGE READ - ' + fname[0])
 
                 # cv2.imwrite('image/test.jpg', cv2.resize(self.get_result_image(image), 1120, 630))
                 # self.pixmap = QPixmap('image/test.jpg')
@@ -212,13 +238,16 @@ class MyWindow(QMainWindow):
                 self.image_label.resize(self.pixmap.width(), self.pixmap.height())
                 self.image_label.move(140, 0)
                 self.sb.showMessage('COMPLETE  --- Show image')
+                self.add_gui_console('COMPLETE  --- Show image')
             else :
+                self.add_gui_console('WARNING   --- Not Found (Image) File')
                 self.sb.showMessage('WARNING   --- Not Found (Image) File')
                 QMessageBox.information(self, 'Warning', 'WARNING   : Not Found (Image) File')
 
 
     def check_model(self):
         if self.model is None :
+            self.add_gui_console('WARNING   --- Not Found Model')
             QMessageBox.information(self, 'Warning', 'Not Found Model')
             return False
         return True
@@ -230,6 +259,7 @@ class MyWindow(QMainWindow):
 
             if fname[0] != '' and fname[0][-4:] == '.mp4':
                 Vid = cv2.VideoCapture(fname[0])
+                self.add_gui_console('MP4 READ - ' + fname[0])
 
                 if Vid.isOpened():
                     while Vid.isOpened():
@@ -254,6 +284,7 @@ class MyWindow(QMainWindow):
 
                             if self.mp4_stop :
                                 print('KeyboardInterrupt : "mp4 stop"')
+                                self.add_gui_console('KeyboardInterrupt : MP4 STOP')
                                 break
 
                         else:
@@ -265,9 +296,22 @@ class MyWindow(QMainWindow):
 
 
     def MP4_Stop(self) :
-        self.mp4_stop = True
-        
-    
+        if self.mp4_stop == False :
+            self.mp4_stop = True
+
+
+    def button_event(self):
+        text = self.line_edit1.text()  # line_edit text 값 가져오기
+        # self.add_gui_console('KeyboardInterrupt : MP4 STOP')
+        self.text_label1.setText(text)  # label에 text 설정하기
+
+
+    def add_gui_console(self, text) :
+        origin_text = self.scroll_label.text()  # line_edit text 값 가져오기
+        text = origin_text + '\n' + 'time - ' + str(time.time() - self.start_time) + '; GUI console : ' + text
+        self.scroll_label.setText(text)  # label에 text 설정하기
+
+
     # def alaram(self) :
     #     def send_email(subject, message, recipient):
     #         # SMTP 서버 정보
@@ -275,7 +319,7 @@ class MyWindow(QMainWindow):
     #         smtp_port = 587
     #         smtp_user = "skaghrl0@gmail.com"
     #         smtp_pass = "gnoukvtchqfvyvpm"
-    # 
+    #
     #         # 이메일 보내기
     #         try:
     #             server = smtplib.SMTP(smtp_server, smtp_port)
@@ -287,11 +331,11 @@ class MyWindow(QMainWindow):
     #             print("Still not detected")
     #         except Exception as e:
     #             print(f"Failed to send email. Error: {e}")
-    # 
+    #
     #     def check_safety_equipment():
     #         # 안전 장구류 미착용 확인 코드
     #         return False
-    # 
+    #
     #     while True:
     #         safety_equipment_detected = check_safety_equipment()
     #         if not safety_equipment_detected == True:
